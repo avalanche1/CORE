fc63f6c74f46011ea4cebff643016410ae032f1c
Kat <avalanche1@mail.ru>
2019-06-18 17:03:50 +0300

    ver 0.4 - sidebar navigation

--- src/components/TreeMenu.js
+++ src/components/TreeMenu.js
@@ -1,64 +1,112 @@
+/* eslint-disable object-property-newline */
 // @flow strict
 import React from "react";
+
 import {Accordion} from "semantic-ui-react";
 
-export function TreeMenu() {
-  return <Accordion panels={getMainSections()} defaultActiveIndex={0} fluid styled />;
+import {pipe} from "../utility-belt/fp/pipe";
+import {select} from "../utility-belt/fp/select";
 
-  function getMainSections() {
+type Props = {
+  set_active_table: Function,
+};
+export function TreeMenu({set_active_table}: Props) {
+  return <Accordion panels={get_main_sections()} defaultActiveIndex={0} fluid styled />;
+  function get_main_sections() {
     return [
       {
         key: "Data Entry",
         title: "Data Entry",
-        content: {content: getDataEntrySections()},
+        content: get_accordion_content("entry"),
       },
       {
         key: "Calculated Data",
         title: "Calculated Data",
-        content: {content: getCalculatedDataSections()},
+        content: get_accordion_content("calc"),
       },
       {
         key: "Reports",
         title: "Reports",
-        content: {content: getReportsSections()},
+        content: get_accordion_content("reports"),
       },
       {
         key: "Forecasts",
         title: "Forecasts",
+        content: get_accordion_content("forecast"),
       },
     ];
   }
-  function getDataEntrySections() {
-    return (
-      <div>
-        <div>DOWNTIME</div>
-        <div>MET</div>
-        <div>CIL</div>
-        <div>STRIP</div>
-        <div>GPOURED</div>
-        <div>GREFINED</div>
-        <div>KPI</div>
-        <div>REAGENT</div>
-        <div>R-STOCKTAKE</div>
-        <div>WATER</div>
-        <div>ORE BLEND</div>
-      </div>
-    );
+
+  // * @exampleInput: "reports"
+  // * @exampleOutput: [{tableName: "PRODUCTION", componentName: ""}]
+  function get_accordion_content(tableType: "entry" | "calc" | "reports" | "forecast") {
+    const result = {
+      // prettier-ignore
+      content: pipe(tableType).thru(
+        get_table_component_map,
+        create_link_list
+      )
+    };
+    return result;
   }
-  function getCalculatedDataSections() {
-    return (
-      <div>
-        <div>DOWNTIME</div>
-        <div>MET</div>
-        <div>GRAPH</div>
-      </div>
-    );
+
+  // * @exampleInput: "reports"
+  // * @exampleOutput: [{tableName: "PRODUCTION", componentName: ""}]
+  function get_table_component_map(
+    tableType: "entry" | "calc" | "reports" | "forecast",
+  ) {
+    const result = select({
+      entry: [
+        {tableName: "DOWNTIME", componentName: "DowntimeTable"},
+        // {tableName: "MET", componentName: ""},
+        // {tableName: "CIL", componentName: ""},
+        // {tableName: "STRIP", componentName: ""},
+        {tableName: "GPOURED", componentName: "GoldPouredTable"},
+        // {tableName: "GREFINED", componentName: ""},
+        // {tableName: "KPI", componentName: ""},
+        // {tableName: "REAGENT", componentName: ""},
+        // {tableName: "R-STOCKTAKE", componentName: ""},
+        // {tableName: "WATER", componentName: ""},
+        // {tableName: "ORE BLEND", componentName: ""},
+      ],
+      calc: [
+        // {tableName: "DOWNTIME", componentName: ""},
+        // {tableName: "MET", componentName: ""},
+        // {tableName: "GRAPH", componentName: ""},
+      ],
+      // reports: [{tableName: "PRODUCTION", componentName: "ProductionReport"}],
+      forecast: [],
+    })([])(tableType);
+    return result;
   }
-  function getReportsSections() {
-    return (
-      <div>
-        <div>PRODUCTION</div>
-      </div>
+
+  // * @exampleInput: [{tableName: "PRODUCTION", componentName: ""}]
+  // * @exampleOutput: list of links from create_link()
+  function create_link_list(
+    componentMapArr: Array<{
+      tableName: string,
+      componentName: string,
+    }>,
+  ) {
+    const list = componentMapArr.map((componentMap, i) => (
+      <div key={i}>{create_link(componentMap)}</div>
+    ));
+    return <div>{list}</div>;
+  }
+
+  // * @exampleInput: {tableName: "PRODUCTION", componentName: "ProductionReport"}
+  // * @exampleInput: ...
+  //  ...<a href={"#"} onClick={onClickHandler("ProductionReport")}>PRODUCTION</a>
+  function create_link(componentMap: {tableName: string, componentName: string}) {
+    const result = (
+      <a href={"#"} onClick={() => onClickHandler(componentMap.componentName)}>
+        {componentMap.tableName}
+      </a>
     );
+    return result;
+
+    function onClickHandler(componentName: string) {
+      set_active_table(componentName);
+    }
   }
 }
